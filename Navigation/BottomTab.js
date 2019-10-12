8856f603996771b0ea77f3851e869b458f4beafa
Alyaa AlOstad <alyaaalostad@hotmail.com>
2019-10-12 09:34:19 +0300

    done

--- Navigation/BottomTab.js
+++ Navigation/BottomTab.js
@@ -4,10 +4,22 @@ import CoffeeTab from "./CoffeeTab";
 import AuthTab from "./AuthTab";
 import OrdersTab from "./OrdersTab";
 
-const BottomTab = createBottomTabNavigator({
-  CoffeeTab: CoffeeTab,
-  OrdersTab: OrdersTab,
-  AuthTab: AuthTab
-});
+const BottomTab = createBottomTabNavigator(
+  {
+    CoffeeTab: CoffeeTab,
+    OrdersTab: OrdersTab,
+    AuthTab: AuthTab
+  },
+  {
+    tabBarOptions: {
+      showLabel: false,
+      style: {
+        backgroundColor: "rgb(20,90,100)"
+      },
+      activeBackgroundColor: "#b8cdd0",
+      inactiveBackgroundColor: "white"
+    }
+  }
+);
 
 export default BottomTab;
